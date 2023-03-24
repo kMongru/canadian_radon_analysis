0a6e611ff9ed65279c857a2264439fa135797a0c
Julia <jgroza24@gmail.com>
2023-03-23 23:33:24 -0400

    finished linear and logistic functions

--- Julia/add-coords.py
+++ Julia/add-coords.py
@@ -4,16 +4,16 @@ import pgeocode
 nomi = pgeocode.Nominatim('ca')
 
 # New columns to store latitude and longitude
-df = pd.read_csv('../data/radon-thoron.csv', encoding='latin-1')
+df = pd.read_csv('./data/radon-concentration.csv', encoding='latin-1')
 df["latitude"] = np.nan
 df["longitude"] = np.nan
 
 for row in df.index:
-    location = nomi.query_postal_code(df['forwardSortationAreaCodes'][row])
+    location = nomi.query_postal_code(df['ForwardSortationAreaCodes'][row])
     df['latitude'][row] = location.latitude
     df['longitude'][row] = location.longitude
 
 # Remove Naan values for plotting purposes
 df = df.dropna()
 
-df.to_csv('radon-thoron.csv', index = False) 
+df.to_csv('radon-concentration.csv', index = False) 
